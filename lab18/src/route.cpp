dd67ff4522b2866ced22091cd280b51728d35e1f
Nathan Borthwick <N0689085@my.ntu.ac.uk>
2018-03-02 19:27:21 +0000

    longitude

--- lab18/src/route.cpp
+++ lab18/src/route.cpp
@@ -78,14 +78,20 @@ degrees Route::maxLatitude() const
 
 degrees Route::minLongitude() const
 {
-    const bool implemented = false;
-    assert(implemented);
+    degrees minLongitude = positions[0].longitude();
+    for (int i = 1; 1 < numPositions(); i++) {
+        if (position[i].longitude() < minLongitude) minLongitude = position[i].longitude();
+    }
+    return minLongitude;
 }
 
 degrees Route::maxLongitude() const
 {
-    const bool implemented = false;
-    assert(implemented);
+    degrees maxLongitude = positions[0].longitude();
+    for (int i = 1; 1 < numPositions(); i++) {
+        if (position[i].longitude() > maxLongitude) maxLongitude = position[i].longitude();
+    }
+    return maxLongitude;
 }
 
 metres Route::minElevation() const
